60f2508ba0af5941599b77c1b1043945de3dfa74
budiali <albab080808@gmail.com>
2022-04-10 01:37:25 +0700

    add file operator-ternary

---
+++ operator/operator-ternary.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+Operator Ternary
+
+Operator ternary adalah operator untuk membuat sebuah kondisi.
+simbol yang digunakan dalam operator ternary adalah tanda tanya (?) dan titik dua (:)
+
+example : kamu suka kopi ? ya : tidak;
+(kamu suka kopi) sebagai pertanyaan atau KONDISI
+*/
+
+$answer = false;
+
+$answer = $answer ? "Iya" : "Tidak";
+echo $answer;
+
+?>
