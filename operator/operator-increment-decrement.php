849e28012439a82caa11e000ab73c2743f698254
budiali <albab080808@gmail.com>
2022-04-10 00:01:53 +0700

    added file operator increment decrement

---
+++ operator/operator-increment-decrement.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+Operator increment dan decrement merupakan operator yang digunakan untuk menambah +1 (tambah satu) dan mengurangi -1 (kurangi dengan satu).
+
+Opertor increment menggunakan simbol ++, sedangkan decrement menggunakan simbol --.
+*/
+
+//example
+
+$score = 5;
+
+$score++;
+$score++;
+$score++;
+
+echo $score;
+
+
+?>
