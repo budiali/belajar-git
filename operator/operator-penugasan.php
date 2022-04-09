2c1f6a3b7eb9f70c0ed8e969b4cda400a1c889c8
budiali <albab080808@gmail.com>
2022-04-09 23:54:40 +0700

    update operator-penugasan

--- operator/operator-penugasan.php
+++ operator/operator-penugasan.php
@@ -1,4 +1,28 @@
 <?php 
 
+// = adalah operator assigment
+// $a = 32;
+
+/*
+pengisian                           =
+pengisian dan penambahan            +=
+pengisian dan pengurangan           -=
+pengisian dan perkalian             *=
+pengisian dan pembagian             /=
+pengisian dan pemangkatan           **=
+pengisian dan sisa bagi / modulo    %=
+pengisian dan penggabungan (string) .=
+*/
+
+/*
+perbedaan operator aritmatika dan operator penugasan
+operator penugasan mengisi nilai dan menghitung nilai dengan operasi aritmatika
+operator aritmatika berfungsi untuk menghitung saja
+*/
+
+$speed = 10;
+$speed = $speed + 10; // => operator aritmatika
+echo "Menggunakan operator Aritmatika : ".$speed."<br>";
+echo "Menggunakan operator Penugasan dan operasi Aritmatika :".$speed += 20; // => oprator penugasan
 
 ?>
