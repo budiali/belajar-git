2264931e54198c2674f1aa4b87f07411e735445d
budiali <albab080808@gmail.com>
2022-04-09 23:22:58 +0700

    rename name file and isi

---
+++ operator/operator-aritmatika.php
@@ -0,0 +1,38 @@
+<?php 
+
+$a = 10;
+$b = 5;
+
+//penjumlahan
+$c = $a + $b; 
+echo "Nilai a = $a, Nilai b = $b <br><br>";
+echo "Hasil penjumlahan : $c";
+echo "<hr>";
+
+//pengurangan
+$c = $a - $b;
+echo "Hasil pengurangan : $c";
+echo "<hr>";
+
+//perkalian
+$c = $a * $b;
+echo "Hasil perkalian : $c";
+echo "<hr>";
+
+//pembagian
+$c = $a / $b;
+echo "Hasil pembagian : $c";
+echo "<hr>";
+
+//pemangkatan
+$c = $a ** $b;
+echo "Hasil pemangkatan : $c";
+echo "<hr>";
+
+//sisa bagi
+$c = $a % $b;
+echo "Hasil sisa bagi : $c";
+echo "<hr>";
+
+
+?>
