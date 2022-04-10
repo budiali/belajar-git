dc4cdb79f65ad8873781c4d9754eb52a76e80892
budiali <albab080808@gmail.com>
2022-04-10 19:09:55 +0700

    add file nested loop

---
+++ perulangan.03/nested-loop.php
@@ -0,0 +1,17 @@
+<?php 
+
+// for($i=0; $i < 5; $i++){
+//     for($j=0; $j < 10; $j++){
+//         echo "perulangan ($i, $j) <br>";
+//     }
+// }
+
+$i = 0;
+while($i < 10){
+    for($j = 0; $j < 10; $j++){
+        echo "perulangan $i, $j<br>";
+    }
+    $i++;
+}
+
+?>
