d3b14f30534f930e0000ae3fa9cce624a404a432
budiali <albab080808@gmail.com>
2022-04-10 16:59:42 +0700

    update percabangan if

--- percabangan/if.php
+++ percabangan/if.php
@@ -1,13 +1,36 @@
 <?php 
-$grade = 78;
-if($grade < 60){
-    echo "Nilai anda $grade (D)";
-} else if($grade <= 70){
-    echo "Nilai anda $grade (C)";
-} else if($grade <= 80){
-    echo "Nilai anda $grade (B)";
-}else{
-    echo "Nilai anda $grade (A)";
+// $grade = 78;
+// if($grade < 60){
+//     echo "Nilai anda $grade (D)";
+// } else if($grade <= 70){
+//     echo "Nilai anda $grade (C)";
+// } else if($grade <= 80){
+//     echo "Nilai anda $grade (B)";
+// }else{
+//     echo "Nilai anda $grade (A)";
+// }
+
+// case study
+
+// $umur = 14;
+// $batas_umur = 17;
+// if($umur > $batas_umur){
+//     echo "Khusus $batas_umur th kebawah, Umur anda sekarang $umur th<br>tidak boleh menonton!";
+// }else{
+//     echo "Silahkan menonton!";
+// }
+
+$mhs = "Rossa";
+$nilai = 75;
+
+if($nilai > 70){
+    echo "Nama : $mhs <br>";
+    echo "Nilai : $nilai <br>";
+    echo "Keterangan : Lulus <br>";
+}else {
+    echo "Nama : $mhs <br>";
+    echo "Nilai : $nilai <br>";
+    echo "Keterangan : Tidak Lulus <br>";
 }
 
 ?>
